f308acceeaf95866f958548ccbbf694b85ac120e
blankdvth <contact@blankdvth.com>
2023-03-15 00:18:34 -0400

    Comment code, add missing semicolons

--- tttlogs.js
+++ tttlogs.js
@@ -15,6 +15,7 @@
   CodeMirror.defineMode('tttlogs', function () {
     return {
       startState: function () {
+        // State variables used to determine where we're at in the stream
         return {
           noTarget: false,
           matchedTime: false,
@@ -29,6 +30,7 @@
       },
       token: function (stream, state) {
         if (stream.match(/^\[\d{2}:\d{2}\]/)) {
+          // Reset state variables
           state.noTarget = false;
           state.matchedTime = true;
           state.isRoundNumber = false;
@@ -41,6 +43,7 @@
           return "ttt-time";
         }
         
+        // Generic irrelevant lines (specific enough that we don't need to section them off to prevent false positives)
         if (stream.match(/^-+(\[?(TTT LOGS)\]?-+)?$/)) {
           stream.skipToEnd();
           return "ttt-irrelevant";
@@ -60,6 +63,7 @@
           return "ttt-traitor-detected";
         }
 
+        // Round number, will highlight the number as time and the rest as irrelevant
         if (stream.match(/TTT Round #/)) {
           state.isRoundNumber = true;
           return "ttt-irrelevant";
@@ -74,6 +78,7 @@
           }
         }
         
+        // Player role match
         if (state.beginPlayerRoleMatch || state.beginTargetRoleMatch) {
           var roleMatch = stream.match(/\((Innocent|Traitor|Detective)\)/i);
           if (roleMatch) {
@@ -88,38 +93,39 @@
             return `ttt-role-${roleMatch[1].toLowerCase()}`;
           }
         }
+        // Action match
         if (state.beginActionMatch) {
-          if (stream.match(/ was /))
+          if (stream.match(/ was /)) // Irrelevant Action Words
             return "ttt-irrelevant";
           var kwMatch = stream.match(/(damaged|killed|tased|identified)/);
-          if (kwMatch) { // Action Keywords
+          if (kwMatch) { // Action Keywords, with target
             state.beginActionMatch = false;
             state.isTarget = true;
             state.beginTargetRoleMatch = true;
             return `ttt-action-${kwMatch[0]}`;
           }
           kwMatch = stream.match(/(purchased)/);
-          if (kwMatch) { // Action Keywords
+          if (kwMatch) { // Action Keywords, no target
             state.beginActionMatch = false;
             state.beginWeaponMatch = true;
             state.noTarget = true;
             return `ttt-action-${kwMatch[0]}`;
           }
         }
-        if (state.beginTargetRoleMatch || state.beginWeaponMatch) {
+        if (state.beginTargetRoleMatch || state.beginWeaponMatch) { // Sectioned Off Irrelevant Words (false-positives if they weren't sectioned off)
           if (stream.match(/ (by|body of|an item from the shop:) /)) { // Irrelevant Action Words
             return "ttt-irrelevant";
           }
         }
         
         if (state.beginDamageMatch) {
-          if (stream.match(/ for /))
+          if (stream.match(/ for /)) // Irrelevant Action Words
             return "ttt-irrelevant";
-          if (stream.match(/\d+ damage/))
+          if (stream.match(/\d+ damage/)) // Damage Amount
             return "ttt-damageamount";
-          if (stream.match(/ \(HEADSHOT\) /))
+          if (stream.match(/ \(HEADSHOT\) /)) // Headshot
             return "ttt-headshot";
-          if (stream.match(/ ?with /)) {
+          if (stream.match(/ ?with /)) { // Irrelevant Word, damage section is over
             state.beginDamageMatch = false;
             state.beginWeaponMatch = true;
             return "ttt-irrelevant";
@@ -128,15 +134,19 @@
         
         if (state.beginWeaponMatch) {
           stream.beginWeaponMatch = false;
-          var m = stream.match(/[^\]]+/)
+          var m = stream.match(/[^\]]+/);
+          // Wallhack is a special case, it's specially formatted
           return (m.includes("Wallhack")) ? "ttt-wallhack" : "ttt-weapon";
         }
         
-        if (!state.matchedTime) {
+        if (!state.matchedTime) { // Irrelevant Lines
           stream.skipToEnd();
-          return null;
+          return "ttt-irrelevant";
         }
-        stream.next()
+
+        // If we haven't found anything, we're probably at a player name
+        // When looking for names, we look for role matches and if they don't match, it's part of the name.
+        stream.next();
         return (state.isTarget) ? "ttt-player-target" : "ttt-player";
       },
     }
